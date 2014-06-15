1f865a147abdb2f3f4e6585b6fc8d15d1b916aee
emilianosorbello <emilianosorbello@gmail.com>
2014-06-15 18:25:39 -0300

    Update probabilidad.py
    Ahora funciona!!! Pero es lento, valores como 20 vs 15 o mas grandes no quiere saber nada. Para eso, estoy implementando agregar elementos a la base

--- probabilidad.py
+++ probabilidad.py
@@ -1,16 +1,30 @@
+from constantes_probabilidad import *
+
 class Probabilidad(object):
-    """Coleccion de motores de probabilidades teoricas"""
-    def __init__(self):
-        self.totales = 0
-    def ataque(self, atacante, atacado, minatk = 1, maxdef = 0):
-        """Sarasa"""
-        #ANDA MAL!!!! No se que le pasa, para ejercitos tipo (2,1) anda, para otros empieza a mandar fruta...
-        if atacado <= maxdef: return 0
-        elif atacante <= minatk: return 1
-        else:
-            atacantereal,atacadoreal = min(atacante,4),min(atacado,3)
-            for chance in CHANCES[(atacantereal,atacadoreal)]:
-                self.totales += CHANCES[(atacantereal,atacadoreal)][chance] * self.ataque(atacante - chance[1], atacado - chance[0], minatk, maxdef)
-        total = self.totales
-        self.totales = 0
-        return total
+	"""Coleccion de motores de probabilidades teoricas"""
+	def __init__(self, ultima_probabilidad = 0):
+		self.ultima_probabilidad = ultima_probabilidad
+	
+	def ataque(self, atacante, atacado, minatk = 1, maxdef = 0):
+		if atacado != int(atacado) or atacante != int(atacante):
+			return 0
+		elif atacado <= maxdef:
+			return 1
+		elif atacante <= minatk:
+			return 0
+		elif (atacante, atacado) in CHANCES_GANAR:
+			self.ultima_probabilidad =  CHANCES_GANAR[(atacante, atacado)]
+			return self.ultima_probabilidad
+		else:
+			atacantereal = min(atacante, 4)
+			atacadoreal = min(atacado, 3)
+			chance = CHANCES[(atacantereal, atacadoreal)]
+			chancekeys = sorted(chance.keys())
+			self.ultima_probabilidad = (chance[chancekeys[0]] * self.ataque(atacante + chancekeys[0][0], atacado + chancekeys[0][1], minatk, maxdef) +
+						    chance[chancekeys[1]] * self.ataque(atacante + chancekeys[1][0], atacado + chancekeys[1][1], minatk, maxdef) +
+						    chance[chancekeys[2]] * self.ataque(atacante + chancekeys[2][0], atacado + chancekeys[2][1], minatk, maxdef) +
+						    chance[chancekeys[3]] * self.ataque(atacante + chancekeys[3][0], atacado + chancekeys[3][1], minatk, maxdef))
+			
+			return self.ultima_probabilidad
+	def ver_ultimo_calculo(self):
+		return self.ultima_probabilidad
