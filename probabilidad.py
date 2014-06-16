0934328f252a88cbb3b0eaab85685e8028674340
emilianosorbello <emilianosorbello@gmail.com>
2014-06-16 17:42:59 -0300

    Optimizacion probabilidad.py
    Ahora si: anda a los super pedos. Probala vos tambien

--- probabilidad.py
+++ probabilidad.py
@@ -1,28 +1,21 @@
 from constantes_probabilidad import *
-from manejo_datos import crear_base, crear_base_condicional, agregar_proba, agregar_proba_condicional
+from manejo_datos import crear_base, crear_base_condicional, agregar_proba, agregar_proba_condicional, actualizar_base, actualizar_base_condicional
 
 class Probabilidad(object):
 	""" Coleccion de motores de probabilidades teoricas."""
 	def __init__(self, ultima_probabilidad = 0):
 		self.ultima_probabilidad = ultima_probabilidad
 	
-	def ataque(self, atacante, atacado, minatk = 1,
-		   maxdef = 0, nombre_archivo = "./bases/base.csv",
-		   nombre_archivo2 = "./bases/base_condicional.csv"):
-		
-		if minatk == 1 and maxdef == 0:
-			base = crear_base(nombre_archivo)
-		else:
-			base = crear_base_condicional(nombre_archivo2)
-		return self._ataque(atacante, atacado, minatk, maxdef, base, nombre_archivo, nombre_archivo2)
 	
-	def _ataque(self, atacante, atacado, minatk, maxdef, base, nombre_archivo, nombre_archivo2):
-                
+	def ataque(self, atacante, atacado, minatk = 1,
+                   maxdef = 0, base1 = crear_base(),
+                   base2 = crear_base_condicional()):
+		
 		""" Devuelve la probabilidad real de reducir
-                los ejercitos enemigos a maxdef atacando
-                mientras los nuestros no lleguen a minatk.
-                ALERTA: Muy lenta en datos no guardados.
-                """
+		los ejercitos enemigos a maxdef atacando
+		mientras los nuestros no lleguen a minatk.
+		ALERTA: Muy lenta en datos no guardados.
+		"""
 		
 		#-BASE-
 		# Destruccion de ramas inutiles del "arbol al infinito".
@@ -34,15 +27,20 @@ class Probabilidad(object):
 		# ...o si fracasamos.
 		if atacante <= minatk:
 			return 0
+
+		# Hay una base de datos para probabilidades con minatk=0 y maxdef=1, y otra para los demas casos.
+		if minatk == 1 and maxdef == 0:
+			base = base1
+		else:
+			base = base2
 		
-		# Hay una base de datos para probabilidades con minatk=0 y maxdef=1, y otra para los demas casos.
-		# Hay una base de datos para probabilidades con minatk=0 y maxdef=1, y otra para los demas casos.
 		if (atacante, atacado) in base:
 			if minatk == 1 and maxdef == 0:
 				self.ultima_probabilidad = base[(atacante, atacado)]
 				return self.ultima_probabilidad
-			self.ultima_probabilidad = base[(atacante, atacado)][(minatk, maxdef)]
-			return self.ultima_probabilidad
+			elif (minatk, maxdef) in base[(atacante, atacado)]:
+				self.ultima_probabilidad = base[(atacante, atacado)][(minatk, maxdef)]
+				return self.ultima_probabilidad
 
 		#-RECURSIVIDAD-
 		# En lugar de generarse una pila de datos simple en la PC,
@@ -52,16 +50,18 @@ class Probabilidad(object):
 		atacadoreal = min(atacado, 3)
 		chance = CHANCES[(atacantereal, atacadoreal)]
 		chancekeys = sorted(chance.keys())
-		self.ultima_probabilidad = (chance[chancekeys[0]] * self._ataque(atacante + chancekeys[0][0], atacado + chancekeys[0][1], minatk, maxdef, base, nombre_archivo, nombre_archivo2) +
-					    chance[chancekeys[1]] * self._ataque(atacante + chancekeys[1][0], atacado + chancekeys[1][1], minatk, maxdef, base, nombre_archivo, nombre_archivo2) +
-					    chance[chancekeys[2]] * self._ataque(atacante + chancekeys[2][0], atacado + chancekeys[2][1], minatk, maxdef, base, nombre_archivo, nombre_archivo2) +
-					    chance[chancekeys[3]] * self._ataque(atacante + chancekeys[3][0], atacado + chancekeys[3][1], minatk, maxdef, base, nombre_archivo, nombre_archivo2))
+		self.ultima_probabilidad = (chance[chancekeys[0]] * self.ataque(atacante + chancekeys[0][0], atacado + chancekeys[0][1], minatk, maxdef, base1, base2) +
+					    chance[chancekeys[1]] * self.ataque(atacante + chancekeys[1][0], atacado + chancekeys[1][1], minatk, maxdef, base1, base2) +
+					    chance[chancekeys[2]] * self.ataque(atacante + chancekeys[2][0], atacado + chancekeys[2][1], minatk, maxdef, base1, base2) +
+					    chance[chancekeys[3]] * self.ataque(atacante + chancekeys[3][0], atacado + chancekeys[3][1], minatk, maxdef, base1, base2))
 
 		#Aqui, luego de realizar los calculos, agrega a la base de datos los resultados nuevos.
 		if minatk == 1 and maxdef == 0:
-			agregar_proba(nombre_archivo, atacante, atacado, self.ultima_probabilidad)
+			actualizar_base(base, atacante, atacado, self.ultima_probabilidad)
+			agregar_proba("./bases/base.csv", atacante, atacado, self.ultima_probabilidad)
 		else:
-			agregar_proba_condicional(nombre_archivo2, atacante, atacado, minatk, maxdef, self.ultima_probabilidad)
+			actualizar_base_condicional(base, atacante, atacado, minatk, maxdef, self.ultima_probabilidad)
+			agregar_proba_condicional("./bases/base_condicional.csv", atacante, atacado, minatk, maxdef, self.ultima_probabilidad)
 		
 		#Devuelvo el elemento aqui mismo, quedando igual guardado en el atributo hasta un nuevo calculo.
 		return self.ultima_probabilidad
@@ -73,4 +73,3 @@ class Probabilidad(object):
 
 #Este de aca esta para hacer pruebas en terminal mas rapido, despues lo borro.
 proba = Probabilidad()
-
