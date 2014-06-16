da37b479e32b631a6aa1209d3291fc8e8087714f
Blaz77 <lugusor@gmail.com>
2014-06-15 22:41:27 -0300

    Retoques
    Espero no haber hecho lío. Cualquier cosa se puede volver hacia atrás.

--- jugador_inteligente.py
+++ jugador_inteligente.py
@@ -9,63 +9,63 @@ class JugadorInteligente(Jugador):
 	(Escribir codigo de aca para abajo)
 	"""
 
-	def _limita_con(self, tablero, pais, paises_ganados_ronda, funcion):
+	def _limita_con(self, tablero, pais, condicion):
 		""" Recibe un pais, devuelve True si 
-		un limite cumple con la condicion.
+		algun limite cumple con la condicion.
 		"""
-		if not es_mi_pais:
+		if not self.es_mi_pais(tablero, pais):
 			raise ValueError("No es tu pais!")
 		for limitrofe in tablero.paises_limitrofes(pais):
-			if funcion(tablero, limitrofe, paises_ganados_ronda):
+			if condicion(tablero, limitrofe):
 				return True
 		return False
 	
-	def es_mi_pais(self, tablero, pais, paises_ganados_ronda):
+	def es_mi_pais(self, tablero, pais):
 		return self.color == tablero.color_pais(pais)
 	
-	def es_enemigo(self,tablero, pais, paises_ganados_ronda):
-		return not self.es_mi_pais(self, tablero, pais, paises_ganados_ronda)
+	def es_enemigo(self, tablero, pais):
+		return not self.es_mi_pais(tablero, pais)
 
-	def es_frontera(self, tablero, pais, paises_ganados_ronda):
+	def es_frontera(self, tablero, pais):
 		""" Devuelve True si el pais limita con algun pais enemigo.
 		"""
-		return _limita_con(self, tablero, pais, paises_ganados_ronda, self.es_enemigo)
+		return self._limita_con(tablero, pais, self.es_enemigo)
 		
-	def es_frontera_unica(self, tablero, pais, paises_ganados_ronda):
-		"""Devuelve True si el pais es frontera y 
+	def es_frontera_unica(self, tablero, pais):
+		""" Devuelve True si el pais es frontera y 
 		no limita con ninguna otra.
 		"""
-		es_frontera = self.es_frontera(self, tablero, pais, paises_ganados_ronda)
+		es_frontera = self.es_frontera(tablero, pais)
 		if not es_frontera:
 			return False
-		return _limita_con(self, tablero, pais, paises_ganados_ronda, self.es_frontera)
+		return not self._limita_con(tablero, pais, self.es_frontera)
 		
-	def es_orden2(self, tablero, pais, paises_ganados_ronda):
+	def es_orden2(self, tablero, pais):
 		""" Devuelve True si el pais no es frontera pero 
 		es limitrofe con alguna de tus fronteras.
 		"""
-		es_frontera = self.es_frontera(self, tablero, pais, paises_ganados_ronda)
+		es_frontera = self.es_frontera(tablero, pais)
 		if es_frontera:
 			return False
-		return _limita_con(self, tablero, pais, paises_ganados_ronda, self.es_frontera)
+		return self._limita_con(tablero, pais, self.es_frontera)
 	
-	def es_orden3(self, tablero, pais, paises_ganados_ronda):
-		""" Devuelve True si el pais no es frontera o 
+	def es_orden3(self, tablero, pais):
+		""" Devuelve True si el pais no es frontera ni 
 		de orden 2 pero limita con alguno de orden 2.
 		"""
-		es_frontera = self.es_frontera(self, tablero, pais, paises_ganados_ronda)
-		es_orden2 = self.es_orden2(self, tablero, pais, paises_ganados_ronda)
+		es_frontera = self.es_frontera(tablero, pais)
+		es_orden2 = self.es_orden2(tablero, pais)
 		if es_frontera or es_orden2:
 			return False
-		return _limita_con(self, tablero, pais, paises_ganados_ronda, self.es_orden2)
+		return self._limita_con(tablero, pais, self.es_orden2)
 	
-	def es_seguro(self, tablero, pais, paises_ganados_ronda):
-		"""Deuvelve True si el pais no puede 
+	def es_seguro(self, tablero, pais):
+		""" Deuvelve True si el pais no puede 
 		ser atacado en el siguiente turno.
 		"""
-		es_frontera = self.es_frontera(self, tablero, pais, paises_ganados_ronda)
-		es_orden2 = self.es_orden2(self, tablero, pais, paises_ganados_ronda)
-		es_orden3 = self.es_orden3(self, tablero, pais, paises_ganados_ronda)
+		es_frontera = self.es_frontera(tablero, pais)
+		es_orden2 = self.es_orden2(tablero, pais)
+		es_orden3 = self.es_orden3(tablero, pais)
 		return not (es_frontera or es_orden2 or es_orden3)
 		
 		
--- simulador.py
+++ simulador.py
@@ -1,33 +1,36 @@
 #from random import randrange,randint
 class Simulador(object):
-	"""Motores de simulacion de probabilidades para situaciones aleatorias"""
+	""" Motores de simulacion de probabilidades para situaciones aleatorias"""
 	def __init__(self, probabilidad = 0):
 		self.probabilidad = probabilidad
 	
-	def ataque_individual(self, dado, atacante, atacado, minatk = 1, maxdef = 0): 
+	def ataque_individual(self, dado, atacante, atacado, minatk = 1, mindef = 0): 
 		""" Recibe un dado, la cantidad de ejercitos de cada lado, 
 		un minimo de ejercitos a conservar, y un minimo de 
 		ejercitos enemigos a destruir. Devuelve True si 
 		consigue destruir el minimo de ejercitos enemigos 
-		o False si no lo logra para cuando  le quedan igual 
+		o False si no lo logra para cuando le quedan igual 
 		o menos ejercitos que el minimo.
 		"""
-		if atacado <= maxdef:
+		# Para este tipo de funciones la recursividad no es muy buena, ya que el
+		# regreso no se aprovecha. Si llega a funcionar lento seria bueno modificarla
+		if atacado <= mindef:
 			return True
 		elif atacante <= minatk:
 			return False
 		else:
-			dado.lanzar_dados(atacante,atacado)
-			return self.ataque_individual(dado, atacante - dado.ejercitos_perdidos_atacante(),atacado - dado.ejercitos_perdidos_atacado(), minatk, maxdef)
+			dado.lanzar_dados(atacante, atacado)
+			return self.ataque_individual(dado, atacante - dado.ejercitos_perdidos_atacante(), \
+					atacado - dado.ejercitos_perdidos_atacado(), minatk, mindef)
 
-	def chances_ataque(self, dado, atacante, atacado, minatk = 1, maxdef = 0, precision = 1000):
+	def chances_ataque(self, dado, atacante, atacado, minatk = 1, mindef = 0, precision = 1000):
 		""" Corre mil veces la funcion ataque_individual, y 
 		devuelve la probabilidad (estimada) de victoria con 
 		esos ejercitos y limites
 		"""
 		ganados = 0
-		for i in range(precision):
-			gane_combate = self.ataque_individual(dado, atacante, atacado, minatk, maxdef)
+		for i in xrange(precision):
+			gane_combate = self.ataque_individual(dado, atacante, atacado, minatk, mindef)
 			if gane_combate:
 				ganados += 1
-		return ganados*1.0/precision
+		return ganados*1.0 / precision
