adbd5ceed2cd5a60e5820c3130f7fc75a4a55440
emilianosorbello <emilianosorbello@gmail.com>
2014-06-16 01:25:52 -0300

    Update manejo_datos.py
    Cambio de nombres feos, distincion entre datos para matar o morir de datos para objetivos mas sofisticados.

--- manejo_datos.py
+++ manejo_datos.py
@@ -1,5 +1,6 @@
 from os import linesep
 import csv
+from constantes_probabilidad import *
 
 def desencadenar(valor):
         ''' Recibe una cadena numerica y la convierte a
@@ -14,7 +15,7 @@ def desencadenar(valor):
         except:
                 raise ValueError("Recibio un valor no numerico!")
 
-def csv_a_diccionario(nombre_archivo):
+def crear_base(nombre_archivo):
         ''' Convierte los datos de nuestro archivo de 
         probabilidades calculadas a un diccionario y 
         lo devuelve.
@@ -35,11 +36,48 @@ def csv_a_diccionario(nombre_archivo):
         finally:
                 archivo.close()
 
-def agregar_probabilidad(archivo, atacante, atacado, probabilidad):
+def agregar_proba(nombre_archivo, atacante, atacado, probabilidad):
         try:
-                abierto = open(archivo, "a")
+                archivo = open(nombre_archivo, "a")
         except:
                 raise RuntimeError("Error al abrir el archivo: No existe o esta siendo leido por otro")
-        linea = ",".join(str(atacante),str(atacado),str(probabilidad))
-        abierto.write(linea+linesep)
-        abierto.close()
+        linea = ",".join((str(atacante),str(atacado),str(probabilidad)))
+        archivo.write(linea+linesep)
+        archivo.close()
+
+def crear_base_condicional(nombre_archivo):
+        ''' Convierte los datos de nuestro archivo de 
+        probabilidades calculadas a un diccionario y 
+        lo devuelve.
+        '''
+        try:
+                archivo = open(nombre_archivo)
+                archivo_csv = csv.reader(archivo)
+        except IOError:
+                raise RuntimeError("Error al abrir el archivo de datos")
+        try:
+                diccionario = {}
+                archivo_csv.next()
+                for linea in archivo_csv:
+                        atacante = desencadenar(linea[0])
+                        atacado = desencadenar(linea[1])
+                        minatk = desencadenar(linea[2])
+                        maxdef = desencadenar(linea[3])
+                        probabilidad = desencadenar(linea[4])
+                        if (atacante, atacado) not in diccionario:
+                                diccionario[(atacante, atacado)] = {}
+                        diccionario[(atacante,atacado)].update({(minatk,maxdef):probabilidad})
+                return diccionario
+        except:
+                raise RuntimeError()
+        finally:
+                archivo.close()
+
+def agregar_proba_condicional(nombre_archivo, atacante, atacado, minatk, maxdef, probabilidad):
+        try:
+                archivo = open(nombre_archivo, "a")
+        except:
+                raise RuntimeError("Error al abrir el archivo: No existe o esta siendo leido por otro")
+        linea = ",".join((str(atacante), str(atacado), str(minatk), str(maxdef), str(probabilidad)))
+        archivo.write(linea+linesep)
+        archivo.close()
