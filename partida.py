fdfde5b0e01c1ca6c8561780250e6fa2e995d62d
Blaz77 <lugusor@gmail.com>
2014-06-19 01:53:56 -0300

    Test de jugadores
    Parece que JugadorInexperto le gana a JugadorAprendiz

--- partida.py
+++ partida.py
@@ -10,9 +10,15 @@ from jugador_inexperto import JugadorInexperto
 # Configuracion de jugadores para la partida.
 # Cuidado de no repetir colores.
 #
+
+# La forma que uso para ver si el jugador gano por suerte o por ser
+# superior es iniciar una partida, cambiar los roles y jugar con la
+# misma semilla. Dejo los jugadores comentados para mas rapidez
 jugadores = [
 	JugadorSuicida(COLOR_NEGRO, 'Jugador 1'),
-	JugadorOfensivo(COLOR_AZUL, 'Jugador 2'),
+	#JugadorInexperto(COLOR_AZUL, 'Jugador 2'),
+	#JugadorAprendiz(COLOR_ROJO, 'Jugador 3'),
+	JugadorAprendiz(COLOR_AZUL, 'Jugador 2'),
 	JugadorInexperto(COLOR_ROJO, 'Jugador 3'),
 	JugadorSuicida(COLOR_ROSA, 'Jugador 4'),
 ]
